577b56f2df7ed9be711868278d29cab61f6f9efe
crav99 <allanvargas000@gmail.com>
2024-01-08 12:47:20 -0600

    Change style on action buttons

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -55,13 +55,13 @@ const Home = () => {
                 <td className="border border-slate-700 rounded-md text-center">
                   <div className="flex justify-center gap-x-4">
                     <Link to={`/books/details/${book._id}`}>
-                      <BsInfoCircle />
+                      <BsInfoCircle className="text-2xl text-green-800" />
                     </Link>
                     <Link to={`/books/update/${book._id}`}>
-                      <AiOutlineEdit />
+                      <AiOutlineEdit className="text-2xl text-yellow-600" />
                     </Link>
                     <Link to={`/books/delete/${book._id}`}>
-                      <MdOutlineDelete />
+                      <MdOutlineDelete className="text-2xl text-red-600" />
                     </Link>
                   </div>
                 </td>
